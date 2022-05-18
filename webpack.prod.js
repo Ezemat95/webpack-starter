332a4e8b2aed2a2afd90f20899d68bd47e155d1d
Ezequiel <Ezzesitodf@gmail.com>
2022-05-18 15:35:03 -0300

    Inicializacion de Proyecto

---
+++ webpack.prod.js
@@ -0,0 +1,84 @@
+const HtmlWebpack          = require('html-webpack-plugin');
+const { loader }           = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin           = require("copy-webpack-plugin");
+
+const Cssminimizer         = require('css-minimizer-webpack-plugin');
+const Terser               = require('terser-webpack-plugin');
+
+
+
+module.exports = {
+        mode: 'production',
+    
+        output: {
+            clean: true,
+            filename: 'main.[contenthash].js'
+        },
+
+
+        module: {
+            rules:  [
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader',
+                    options: {
+                        sources: false
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /styles.css$/,
+                    use: [ 'style-loader', 'css-loader']
+                },
+                {
+                    test: /styles.css$/,
+                    use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+                },
+
+                {
+                   test: /\.(png|jpg?g|gif)$/,
+                   loader: 'file-loader'
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                      loader: "babel-loader",
+                      options: {
+                        presets: ['@babel/preset-env']
+                      }
+                    }
+                }
+            ]
+         },
+
+         optimization: {
+            minimize: true,
+            minimizer: [
+                new Cssminimizer(),
+                new Terser(),
+            ]
+         },
+
+
+         plugins: [
+            new HtmlWebpack({
+                title: 'Mi webpack App',
+                //filename: 'index.html',
+                template: './src/index.html',
+
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].[fullhash].css',
+                ignoreOrder: false  
+            }),
+            
+            new CopyPlugin({
+                patterns: [
+                    {from: 'src/assets/', to: 'assets/' }
+                ]
+            })
+
+         ]
+}
